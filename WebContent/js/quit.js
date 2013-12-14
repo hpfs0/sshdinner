3cdc8af61d0302cbbcd001aed70a0356d97facb4
Mcgrean0519 <389314570@qq.com>
2013-12-14 18:53:21 +0800

    非IE浏览器用户正常退出提醒
    Signed-off-by: Mcgrean0519 <389314570@qq.com>

--- WebContent/js/quit.js
+++ WebContent/js/quit.js
@@ -26,3 +26,17 @@ window.onunload = function unLoad( e ) {
         }
 	}
 };
+
+// 当用户使用浏览器不是IE时，提醒用户正常退出系统
+// TODO 待改善
+if ((navigator.userAgent.indexOf('MSIE') >= 0) 
+		&& (navigator.userAgent.indexOf('Opera') < 0))
+{
+	//alert("您使用的是IE系统");
+}else{
+	var t=setTimeout("alert('您使用的不是IE浏览器，请注意正常退出系统，以方便您下次能正常登陆,谢谢!')",3000);
+	
+};
+
+
+  
