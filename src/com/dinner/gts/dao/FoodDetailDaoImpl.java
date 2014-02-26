cb4cdb87cd57e5ca22ad595fd9ddfc66fb068107
Mcgrean0519 <389314570@qq.com>
2014-02-26 23:51:43 +0800

    订菜详细画面试实装
    Signed-off-by: Mcgrean0519 <389314570@qq.com>

--- src/com/dinner/gts/dao/FoodDetailDaoImpl.java
+++ src/com/dinner/gts/dao/FoodDetailDaoImpl.java
@@ -5,22 +5,36 @@ package com.dinner.gts.dao;
 
 import java.util.List;
 
-import com.dinner.gts.model.FoodDetail;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 
+import com.dinner.gts.common.CommonSqlConst;
+import com.dinner.gts.common.CommonUtil;
+import com.dinner.gts.model.FoodDetail;
 
 /**
  * @author Administrator
- *
  */
 public class FoodDetailDaoImpl implements FoodDetailDao {
 
-    /* (non-Javadoc)
-     * @see com.dinner.gts.dao.FoodDetailDao#getAllFoodDetails()
-     */
+    // Session初始化
+    private Session session = CommonUtil.getSessionFactory().openSession();
+
     @Override
     public List<FoodDetail> getAllFoodDetails(FoodDetail foodDetailForSelectQuery) {
-        // TODO Auto-generated method stub
-        return null;
+        SQLQuery query = session.createSQLQuery(CommonSqlConst.COMMON_SQL_O15);
+        query.addEntity(FoodDetail.class);
+        // 设置参数
+        query.setString(0, foodDetailForSelectQuery.getFoodName());
+        query.setDouble(1, foodDetailForSelectQuery.getFoodMemberPriceFrom());
+        query.setDouble(2, foodDetailForSelectQuery.getFoodMemberPriceTo());
+        query.setString(3, foodDetailForSelectQuery.getPreferentialStatus());
+        query.setString(4, foodDetailForSelectQuery.getFoodCuisineKind());
+        query.setString(5, foodDetailForSelectQuery.getFoodPungencyDegree());
+        @SuppressWarnings("unchecked")
+        List<FoodDetail> resultFoodDetail = query.list();
+        // session关闭
+        CommonUtil.closeSession(session);
+        return resultFoodDetail;
     }
-
 }
