59379fd6f204dc10eccd22f75e79837cefe0156c
Mcgrean0519 <389314570@qq.com>
2014-04-03 00:30:33 +0800

    菜品详细画面的动态查询初时装
    Signed-off-by: Mcgrean0519 <389314570@qq.com>

--- src/com/dinner/gts/dao/FoodDetailDaoImpl.java
+++ src/com/dinner/gts/dao/FoodDetailDaoImpl.java
@@ -3,38 +3,59 @@
  */
 package com.dinner.gts.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.SQLQuery;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 
-import com.dinner.gts.common.CommonSqlConst;
 import com.dinner.gts.common.CommonUtil;
 import com.dinner.gts.model.FoodDetail;
 
 /**
- * @author Administrator
+ * @author yux
  */
 public class FoodDetailDaoImpl implements FoodDetailDao {
 
     // Session初始化
     private Session session = CommonUtil.getSessionFactory().openSession();
 
+    // DetachedCriteria实现动态查询
+    DetachedCriteria detachedCrit =
+            DetachedCriteria.forClass(FoodDetail.class);
+
+    @SuppressWarnings("unchecked")
     @Override
     public List<FoodDetail> getAllFoodDetails(FoodDetail foodDetailForSelectQuery) {
-        SQLQuery query = session.createSQLQuery(CommonSqlConst.COMMON_SQL_O15);
-        query.addEntity(FoodDetail.class);
-        // 设置参数
-        query.setString(0, "%" + foodDetailForSelectQuery.getFoodName() + "%");
-        query.setDouble(1, foodDetailForSelectQuery.getFoodMemberPriceFrom());
-        query.setDouble(2, foodDetailForSelectQuery.getFoodMemberPriceTo());
-        query.setString(3, foodDetailForSelectQuery.getPreferentialStatus());
-        query.setString(4, foodDetailForSelectQuery.getFoodCuisineKind());
-        query.setString(5, foodDetailForSelectQuery.getFoodPungencyDegree());
-        @SuppressWarnings("unchecked")
-        List<FoodDetail> resultFoodDetail = query.list();
-        // session关闭
-        CommonUtil.closeSession(session);
-        return resultFoodDetail;
+        Criteria criteria = detachedCrit.getExecutableCriteria(session);
+        if (null != foodDetailForSelectQuery.getFoodName()
+                && foodDetailForSelectQuery.getFoodName().trim().length() > 0) {
+            detachedCrit.add(Restrictions.like("foodName", "'"
+                    + foodDetailForSelectQuery.getFoodName()
+                            .trim() + "'", MatchMode.ANYWHERE));
+        }
+        if (foodDetailForSelectQuery.getFoodMemberPriceFrom() > 0.0) {
+            detachedCrit.add(Restrictions.ge("foodMemberPrice",
+                    foodDetailForSelectQuery.getFoodMemberPriceFrom()));
+        }
+        if (foodDetailForSelectQuery.getFoodMemberPriceTo() > 0.0) {
+            detachedCrit.add(Restrictions.le("foodMemberPrice",
+                    foodDetailForSelectQuery.getFoodMemberPriceTo()));
+        }
+        if (null != foodDetailForSelectQuery.getPreferentialStatus()) {
+            detachedCrit.add(Restrictions.eq("preferentialStatus",
+                    foodDetailForSelectQuery.getPreferentialStatus()));
+        }
+        if (null != foodDetailForSelectQuery.getFoodCuisineKind()) {
+            detachedCrit.add(Restrictions.eq("foodPungencyDegree",
+                    foodDetailForSelectQuery.getFoodPungencyDegree()));
+        }
+        List<FoodDetail> foodDetailsList = new ArrayList<FoodDetail>();
+
+        foodDetailsList = criteria.list();
+        return foodDetailsList;
     }
 }
